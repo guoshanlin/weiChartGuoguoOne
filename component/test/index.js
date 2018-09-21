e129e775f28df289bceeda0fe7c83e3f0d156c66
guosl <guosl@coreware.com>
2018-09-21 15:53:49 +0800

    1、微信小程序练习

---
+++ component/test/index.js
@@ -0,0 +1,25 @@
+// component/test/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    testName: {
+      type: String,  //限制类型为字符
+      default: null   //默认值为null
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      aaa: "组件"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
